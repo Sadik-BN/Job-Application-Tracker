c544bca2b400f0f2802467fa3f099ae4c92375b5
Sadik-BN <sadikonline4@gmail.com>
2026-03-03 05:41:55 +0600

    Fully functionable toggling

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,11 +1,48 @@
-let totalJob= 8;
-let interview=0;
-let rejected=0;
+let totalJob = 8;
+let interview = 0;
+let rejected = 0;
 
-function updateJobs(){
+let allSection = document.getElementById("all-sec");
+let interviewSection = document.getElementById("interview-sec");
+let rejectedSection = document.getElementById("rejected-sec");
+let emptySection = document.getElementById("empty");
+
+let allBtn = document.getElementById("all-btn");
+let interviewBtn = document.getElementById("interview-btn");
+let rejectedBtn = document.getElementById("rejected-btn");
+
+function updateJobs() {
     let dashCard = document.querySelectorAll(".dash-card h3");
     dashCard[0].innerText = `${totalJob}`;
     dashCard[1].innerText = `${interview}`;
     dashCard[2].innerText = `${rejected}`;
+
+    if (allSection.hidden === false) {
+        document.getElementById("page-job-count-all").innerText = `${totalJob} Jobs`;
+        if (totalJob == 0) {
+            emptySection.hidden = false;
+        }
+        else {
+            emptySection.hidden = true;
+        }
+    }
+    if (interviewSection.hidden === false) {
+        document.getElementById("page-job-count-all").innerText = `${interview} of ${totalJob} Jobs`;
+        if (interview == 0) {
+            emptySection.hidden = false;
+        }
+        else {
+            emptySection.hidden = true;
+        }
+    }
+    if (rejectedSection.hidden === false) {
+        document.getElementById("page-job-count-all").innerText = `${rejected} of ${totalJob} Jobs`;
+        if (rejected == 0) {
+            emptySection.hidden = false;
+        }
+        else {
+            emptySection.hidden = true;
+        }
+    }
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,49 @@
 updateJobs();
+
+let deleteButtons = document.querySelectorAll(".dlt-btn");
+
+for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", function () {
+        let card = deleteButtons[i].parentElement.parentElement;
+        card.remove();
+        totalJob--;
+        if (totalJob == 0) {
+            document.getElementById("empty").hidden = false;
+        }
+        updateJobs();
+    });
+}
+
+allBtn.addEventListener("click", function () {
+    allSection.hidden = false;
+    interviewSection.hidden = true;
+    rejectedSection.hidden = true;
+
+    allBtn.classList.remove("btn-soft");
+    interviewBtn.classList.add("btn-soft");
+    rejectedBtn.classList.add("btn-soft");
+
+    updateJobs();
+});
+document.getElementById("interview-btn").addEventListener("click", function () {
+    document.getElementById("all-sec").hidden = true;
+    document.getElementById("interview-sec").hidden = false;
+    document.getElementById("rejected-sec").hidden = true;
+
+    document.getElementById("all-btn").classList.add("btn-soft");
+    document.getElementById("interview-btn").classList.remove("btn-soft");
+    document.getElementById("rejected-btn").classList.add("btn-soft");
+
+    updateJobs();
+});
+document.getElementById("rejected-btn").addEventListener("click", function () {
+    document.getElementById("all-sec").hidden = true;
+    document.getElementById("interview-sec").hidden = true;
+    document.getElementById("rejected-sec").hidden = false;
+
+    document.getElementById("all-btn").classList.add("btn-soft");
+    document.getElementById("interview-btn").classList.add("btn-soft");
+    document.getElementById("rejected-btn").classList.remove("btn-soft");
+
+    updateJobs();
+});
